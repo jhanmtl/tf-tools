f759351b98a6d9b1f82c2aaabec3303d1d88a622
jhanmtl <jing.han@mail.mcgill.ca>
2020-07-16 13:05:24 -0400

    started gitignore, load_glove on nlp

--- NLP.py
+++ NLP.py
@@ -7,3 +7,17 @@ Created on Thu Jul 16 11:36:40 2020
 """
 
 
+import tensorflow as tf
+import numpy as np
+
+def load_glove(glove_path):
+    glove_lookup = {};
+    with open(glove_path) as f:
+        for line in f:
+            values = line.split();
+            word = values[0];
+            coefs = np.asarray(values[1:], dtype='float32');
+            glove_lookup[word] = coefs;
+    
+    return glove_lookup
+
